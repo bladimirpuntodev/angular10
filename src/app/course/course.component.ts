aa5f13ddcc2674ebd731d1b2fbad62a9f2ca8463
Bladimir Ventura <bventura@techupperu.com>
2020-07-26 12:34:19 -0500

    feat(course): Angular fundamentals

---
+++ src/app/course/course.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css']
+})
+export class CourseComponent implements OnInit {
+
+  title = 'Course 3';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
